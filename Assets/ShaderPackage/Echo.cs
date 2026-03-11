46d7deb36d4569bddf36e44f7648e0ec2eaec155
Beeeq <vanbuurenelias@gmail.com>
2026-03-11 14:55:39 +0100

    add packages

---
+++ Assets/ShaderPackage/Echo.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+
+public class Echo : MonoBehaviour
+{
+    public GameObject echo;
+
+    public float duration = 30;
+    public float size = 500;
+    public float timer;
+
+    public bool timerActive = false;
+
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            Shriek();
+        }
+
+        if (timerActive)
+        {
+            timer += Time.deltaTime;
+            if (timer >= duration)
+            {
+                timer = 0f;
+                timerActive = false;
+                echo.SetActive(false);
+            }
+        }
+    }
+
+    private void Shriek()
+    {
+        echo.SetActive(true);
+        timerActive = true;
+    }
+}
