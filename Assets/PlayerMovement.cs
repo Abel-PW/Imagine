a08ad5abc3b4a9b9ae19a5cab67756ef8d320883
Abel Pauw <abelpauw@gmail.com>
2026-03-12 04:40:49 +0100

    playtest last bits

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -5,21 +5,28 @@ public class PlayerMovement : MonoBehaviour
 {
     private float xPos, yPos;
 
+    private Rigidbody rb;
+
+    private Transform playerTransform;
+
     [SerializeField]
-    private float flySpeed;
+    private float flySpeed, mouseSpeed;
 
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
+        playerTransform = GetComponent<Transform>();
+
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector3 playerDirection = transform.position;
-        Rigidbody rb = GetComponent<Rigidbody>();
+        float mouseX = Input.GetAxis("Mouse X") * mouseSpeed * Time.deltaTime;
+
         if (Input.GetKey("w")) rb.AddForce(transform.forward * flySpeed);
 
         if (Input.GetKey("s")) rb.AddForce(transform.forward * -flySpeed);
@@ -27,6 +34,12 @@ public class PlayerMovement : MonoBehaviour
         if (Input.GetKey("a")) rb.AddForce(transform.right * -flySpeed);
 
         if (Input.GetKey("d")) rb.AddForce(transform.right * flySpeed);
+
+        if (Input.GetKey("space")) rb.AddForce(transform.up * flySpeed);
+
+        if (Input.GetKey(KeyCode.LeftControl)) rb.AddForce(transform.up * -flySpeed);
+
+        playerTransform.Rotate(Vector3.up * mouseX);
 
     }
 }
